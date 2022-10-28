4d7858caff0d0a5309ae4a8e046c89dd8d1e8448
kgwSpace <115144167+kgwSpace@users.noreply.github.com>
2022-10-28 18:02:06 +0900

    Update Tmain.java

--- Tmain.java
+++ Tmain.java
@@ -1,41 +1,22 @@
 
 public class Tmain {
-	private static int count;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	public static void main(String[] args) {
 		
 		ListContainer list = new ListContainer();
 		
-		list.insertNode(new Node(1,"abc"));
-		list.insertNode(new Node(2,"abcd"));
+		list.insertNode(new Node(11,"a"));
+		list.insertNode(new Node(22,"b"));
+		list.insertNode(new Node(33,"c"));
+		list.insertNode(new Node(44,"d"));
+		list.insertNode(2,new Node(55,"e"));
 		
-		list.deleteNode(0);
 		
-		System.out.println(list.getNodeCount());
+	//	list.deleteNodeByIntValue(44);
+		
+		
+		
+		list.printNodeContainer();
 	}
-	
-	
-	//내가 가지고 있는 값이 최대값보다 큰 값이 들어오면 맨뒤,
-	//-단위는 false;
-	//insertNode
-	//InsertNode(3, newNode) 이런 식으로 만들어보자
-	public static boolean iinsertNode(Node head, int index) {
-		if(count <= index) {
-			insertNode(head, new Node(33, null));
-		}
-		
-		return true;
-	}
-	
 	
 }
